815ff771c49d5dbcc369fcea683ebc084114d29d
99002631 <71481233+99002631@users.noreply.github.com>
2020-11-19 15:46:59 +0530

    Update requestApiCheck.py

--- requestApi/requestApiCheck.py
+++ requestApi/requestApiCheck.py
@@ -3,7 +3,3 @@ import requests
 response = requests.get("http://google.com")
 print(response.status_code)
 print(response)
-
-response = requests.get("http://api.open-notify.org/astros.json")
-print(response.status_code)
-print(response.json())
